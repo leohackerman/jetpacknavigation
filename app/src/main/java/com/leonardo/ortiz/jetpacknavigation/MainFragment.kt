1dbc670cc46152558b9f8e2ffad52d1833255d58
Leonardo <ortiz.morales.leonardo@gmail.com>
2018-08-29 10:33:00 -0600

    Added transition to details

--- app/src/main/java/com/leonardo/ortiz/jetpacknavigation/MainFragment.kt
+++ app/src/main/java/com/leonardo/ortiz/jetpacknavigation/MainFragment.kt
@@ -6,12 +6,11 @@ import android.support.v4.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.navigation.fragment.findNavController
+import kotlinx.android.synthetic.main.fragment_main.*
 
 
-// TODO: Rename parameter arguments, choose names that match
-// the fragment initialization parameters, e.g. ARG_ITEM_NUMBER
-private const val ARG_PARAM1 = "param1"
-private const val ARG_PARAM2 = "param2"
+
 
 /**
  * A simple [Fragment] subclass.
@@ -23,6 +22,13 @@ class MainFragment : Fragment() {
                               savedInstanceState: Bundle?): View? {
         // Inflate the layout for this fragment
         return inflater.inflate(R.layout.fragment_main, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        btn_details.setOnClickListener {
+            findNavController().navigate(R.id.action_mainFragment_to_detailFragment)
+        }
     }
 
 
