dfc60311da3ce0b9f6409507af00e28a6239ed9c
Leonardo <ortiz.morales.leonardo@gmail.com>
2018-08-29 15:38:43 -0600

    Added slide transitions

--- app/src/main/java/com/leonardo/ortiz/jetpacknavigation/MainActivity.kt
+++ app/src/main/java/com/leonardo/ortiz/jetpacknavigation/MainActivity.kt
@@ -2,11 +2,24 @@ package com.leonardo.ortiz.jetpacknavigation
 
 import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import androidx.navigation.findNavController
+import androidx.navigation.ui.setupActionBarWithNavController
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        val navController = findNavController(R.id.nav_host_fragment)
+
+
+        setSupportActionBar(findViewById(R.id.main_toolbar))
+        setupActionBarWithNavController(navController)
+
     }
+
+    override fun onSupportNavigateUp() = findNavController(R.id.nav_host_fragment).navigateUp()
+
+
 }
